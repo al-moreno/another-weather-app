6db573b3108109f0927b996124e87d20f879b97f
Ana Moreno <ana.moreno@codeup.com>
2022-10-18 07:54:51 -0500

    startig code

---
+++ app.js
@@ -0,0 +1,38 @@
+const { timeLog } = require('console');
+const express = require('express');
+const https = require('https');
+
+const app = express();
+
+
+
+
+app.get('/', function (req, res) {
+    const url = 'https://api.openweathermap.org/data/2.5/forecast?q=austin&appid=28192cc5dd81f85bcfd688d592d9a8ab&units=imperial';
+
+    https.get(url, function (response) {
+        console.log(response.statusCode);
+
+        response.on('data', function (data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.list[0].main;
+            console.log(temp);
+        })
+    })
+    res.send("Server Is up and running");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, function () {
+    console.log("Server listening on port 3000!");
+});
