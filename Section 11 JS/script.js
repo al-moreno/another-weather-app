86c5000d6c200f1f4a916b1434976a2875704c5c
Ana Moreno <ana.moreno@codeup.com>
2022-10-21 07:45:12 -0500

    tracking weather app

---
+++ Section 11 JS/script.js
@@ -0,0 +1,6 @@
+alert('hello');
+
+document.querySelector('h1').innerHTML = "Good Bye";
+document.querySelector('input').click();
+
+document.getElementsByTagName('li')[2].style.color ='purple';
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { timeLog } = require('console');
+
 const express = require('express');
 const https = require('https');
 
@@ -8,10 +8,11 @@ const app = express();
 let key = "28192cc5dd81f85bcfd688d592d9a8ab";
 
 app.get('/', function (req, res) {
+
     const url = `https://api.openweathermap.org/data/2.5/forecast?q=austin&appid=${key}&units=imperial`;
 
     https.get(url, function (response) {
-        console.log(response.statusCode);
+        console.log(response);
 
         response.on('data', function (data) {
             const weatherData = JSON.parse(data);
